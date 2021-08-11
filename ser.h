3a7046cc214bb8ae14fe3b38d923030064d9b5d6
yclchuxue <3136165538@qq.com>
2021-08-11 14:46:36 +0800

    服务器函数头文件

--- ser.h
+++ ser.h
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <mariadb/mysql.h>
+#include <mysql/mysql.h>
 #include <sys/types.h>
 #include <sys/socket.h>
 #include <netdb.h>
